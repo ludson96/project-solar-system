d7e1593853d7bddd5edbfa3c5815248776acba1a
Ludson <ludson_ps27@hotmail.com>
2022-07-20 17:12:36 -0300

    Requisito 5

---
+++ src/components/PlanetCard.js
@@ -0,0 +1,24 @@
+import { string } from 'prop-types';
+import React from 'react';
+
+class PlanetCard extends React.Component {
+  render() {
+    const { planetName } = this.props;
+    const { planetImage } = this.props;
+    return (
+      <div data-testid="planet-card">
+        <p data-testid="planet-name">
+          {planetName}
+        </p>
+        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+      </div>
+    );
+  }
+}
+
+PlanetCard.propTypes = {
+  planetName: string.isRequired,
+  planetImage: string.isRequired,
+};
+
+export default PlanetCard;
--- src/components/Title.js
+++ src/components/Title.js
@@ -3,10 +3,15 @@ import React from 'react';
 class Title extends React.Component {
   render() {
     const { headline } = this.props;
+    console.log('teste', headline);
     return (
       <h2>{headline}</h2>
     );
   }
 }
+
+Title.propTypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
