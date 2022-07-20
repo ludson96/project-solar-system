e586fe50cf25158751d6b285738f028ba2751235
Ludson <ludson_ps27@hotmail.com>
2022-07-20 18:56:05 -0300

    Requisito 8 e 9

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from './components/Header';
 import SolarSystem from './components/SolarSystem';
-import Missions from './components/Missions';
+import Missions from './components/Missions.js';
 
 class App extends React.Component {
   render() {
